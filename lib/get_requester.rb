8cab9ee78ea61e90cb3ea86ca0673f15a551d85a
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-17 11:39:08 -0700

    i forgot how to Ruby

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    attr_accessor :url
+
+    def initialize url
+        @url = url
+    end
+    
+    def get_response_body
+        # binding.pry
+        uri = URI.parse(self.url)
+        # binding.pry
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        JSON.parse(self.get_response_body)
+        # data
+    end
+
+end
